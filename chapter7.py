d80b8ea57b551267ae588f1a7beaf0e5f9934107
Daniel Prieto <dani@iMac-de-Daniel.local>
2019-04-26 00:12:21 -0400

    excercices 26 of april

--- chapter7.py
+++ chapter7.py
@@ -22,3 +22,17 @@
 #
 # if numbers_in_order == sorted(numbers):
 #     print("The lists are not equal")
+menu = []
+menu.append(["egg","spam","bacon"])
+menu.append(["egg","sausage","bacon"])
+menu.append(["egg","spam"])
+menu.append(["egg","bacon","spam"])
+menu.append(["egg","bacon","sausage","spam"])
+menu.append(["spam","bacon","sausage","spam"])
+menu.append(["spam","egg","spam","spam","bacon","spam"])
+menu.append(["spam","egg","sausage","spam"])
+
+for meal in menu:
+    if not "spam" in meal:
+        for plato in meal:
+            print(plato)
